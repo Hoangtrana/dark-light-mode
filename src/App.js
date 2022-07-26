38a386963119fe93f8c41f77b198caafc8bd0ec5
HoangT <ac5217@student.jamk.fi>
2022-07-27 00:19:48 +0300

    adding dice component and style

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Dice from "./Dice";
+
+function App() {
+  return (
+    <main className="main">
+      <container className="main--container">
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+        <Dice number="1" />
+      </container>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Dice.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dice(props) {
+  return <div className="dice">{props.number}</div>;
+}
+
+export default Dice;
